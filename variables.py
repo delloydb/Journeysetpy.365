259da0e83bae6214bb2a2ecf6650fd13495a26f4
Lloyd <lloydbrownjnr@gmail.com>
2025-10-29 14:19:46 +0300

    variables.py

---
+++ variables.py
@@ -0,0 +1,7 @@
+# learning variables in python programming
+# variable is a named location used to store data in memory.
+# This file contains various types of variables and their usage in Python.
+# variable types: integer, float, string, boolean, list, tuple, dictionary
+# variable naming conventions: snake_case, camelCase, PascalCase
+# variable scope: local, global, nonlocal
+
