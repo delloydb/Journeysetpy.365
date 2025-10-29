202eb5d10958323b8fe556d6932b0c901ce36590
Lloyd <lloydbrownjnr@gmail.com>
2025-10-29 15:42:52 +0300

    input().py

---
+++ input.py
@@ -0,0 +1,11 @@
+# learning input function in python programming
+# The input() function allows user input from the console.
+# This file demonstrates how to use the input() function to get user data.
+# Example: Getting user's name and age
+# Getting user's name
+user_name = input("Enter your name: ")
+print("Hello, " + user_name + "!")
+
+# Getting user's age
+user_age = input("Enter your age: ")
+print("You are " + user_age + " years old.")
