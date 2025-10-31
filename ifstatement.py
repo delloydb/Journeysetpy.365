7a94cc6a63ab5e019a0ee7e87d7cfe08e1dcb84a
Lloyd <lloydbrownjnr@gmail.com>
2025-10-31 15:10:10 +0300

    if statement conditions example in python

---
+++ ifstatement.py
@@ -0,0 +1,14 @@
+# learning if statements in python programming
+# This file demonstrates the use of if statements in Python.
+# Example: Checking if a number is positive, negative, or zero.
+
+# Getting a number from the user
+number = float(input("Enter a number: "))
+
+# Checking if the number is positive, negative, or zero
+if number > 0:
+    print("number is positive.")
+elif number < 0:
+    print("number is negative.")
+else:
+    print("number is zero.")
