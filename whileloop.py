2ab9e1b9e850bb2f0616202d96e84110e1b865c2
Lloyd <lloydbrownjnr@gmail.com>
2025-10-31 15:42:11 +0300

    while loop to print patterns

--- whileloop.py
+++ whileloop.py
@@ -2,13 +2,7 @@
 # This file demonstrates the use of while loops in Python.
 # Example: Printing numbers from 1 to n using a while loop.
 
-# Getting a number from the user
-n = int(input("Enter a positive integer: "))
-# Initializing a counter variable
-counter = 1
-# Using a while loop to print numbers from 1 to n
-while counter <= n:
-    print(counter)
-    counter += 1
-# Incrementing the counter variable
-print("Finished printing numbers from 1 to", n)
+i = 1
+while i <= 19:
+    print("&" * i)
+    i += 1
