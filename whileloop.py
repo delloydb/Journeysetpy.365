4f133b54590581c388fcb44f3aa8ecdf0e8a2e09
Lloyd <lloydbrownjnr@gmail.com>
2025-10-31 15:33:58 +0300

    while loop.py

---
+++ whileloop.py
@@ -0,0 +1,14 @@
+# learning while loops in python programming
+# This file demonstrates the use of while loops in Python.
+# Example: Printing numbers from 1 to n using a while loop.
+
+# Getting a number from the user
+n = int(input("Enter a positive integer: "))
+# Initializing a counter variable
+counter = 1
+# Using a while loop to print numbers from 1 to n
+while counter <= n:
+    print(counter)
+    counter += 1
+# Incrementing the counter variable
+print("Finished printing numbers from 1 to", n)
