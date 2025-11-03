fd269571b1f12a9623dfcf5b1fc595f71ec7984d
Lloyd <lloydbrownjnr@gmail.com>
2025-11-03 23:51:55 +0300

    for loop learning with python examples

--- forloop.py
+++ forloop.py
@@ -2,3 +2,11 @@
 
 for item in range(5):
     print(item)
+
+# example 2: calculating total price from a list of prices
+
+prices = [10, 20, 30, 40, 50]
+total = 0
+for price in prices:
+    total += price
+print("Total price:", total)
