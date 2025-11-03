f99d7ec0601c7cfa262f4e2e9bbf50a60b7e91db
Lloyd <lloydbrownjnr@gmail.com>
2025-11-04 00:03:25 +0300

    lists in python

---
+++ lists.py
@@ -0,0 +1,26 @@
+# learning lists in python programming
+# example 1: creating and accessing a list
+fruits = ["apple", "banana", "cherry", "date"]
+print("Fruits list:", fruits)
+print("First fruit:", fruits[0])  # Accessing the first item
+print("Last fruit:", fruits[-1])  # Accessing the last item
+
+# example 2: modifying a list
+fruits[1] = "blueberry"  # Changing 'banana' to 'blueberry'
+print("Modified fruits list:", fruits)
+
+# example 3: adding and removing items
+fruits.append("elderberry")  # Adding a new fruit
+print("After appending:", fruits)
+fruits.remove("cherry")  # Removing 'cherry'
+print("After removing cherry:", fruits)
+# example 4: iterating through a list
+print("Fruits in the list:")
+for fruit in fruits:
+    print(fruit)
+# example 5: list methods
+print("Number of fruits in the list:", len(fruits))  # Length of the list
+fruits.sort()  # Sorting the list
+print("Sorted fruits list:", fruits)
+fruits.reverse()  # Reversing the list
+print("Reversed fruits list:", fruits)
