ae1aef384dae2be122e3966ef357136ddd5a2ba5
Lloyd <lloydbrownjnr@gmail.com>
2025-11-05 17:54:59 +0300

    func(0);tion.py

---
+++ functions.py
@@ -0,0 +1,4 @@
+# learning functions in python
+# fucntions are a block of code which only runs when it is called and have specific tasks
+# functions help to break our program into smaller and modular chunks
+
