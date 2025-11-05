fb435b156dea7c4c5a0b17e375b2ea9d6622f5ae
Lloyd <lloydbrownjnr@gmail.com>
2025-11-05 18:01:21 +0300

    fucntions basics in python

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@
 # fucntions are a block of code which only runs when it is called and have specific tasks
 # functions help to break our program into smaller and modular chunks
 
+# def is used to define a function, without which function won't work
+
 
 def greet_user():
     print("Hello, welcome to our application!")
