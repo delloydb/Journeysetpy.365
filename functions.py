d664ac2658322aa4a200d501c3e86cd3edeaebf9
Lloyd <lloydbrownjnr@gmail.com>
2025-11-05 17:58:17 +0300

    func():

--- functions.py
+++ functions.py
@@ -2,3 +2,11 @@
 # fucntions are a block of code which only runs when it is called and have specific tasks
 # functions help to break our program into smaller and modular chunks
 
+
+def greet_user():
+    print("Hello, welcome to our application!")
+    print("We hope you have a great experience.")
+
+
+# calling the function
+greet_user()
