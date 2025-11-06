fa427f2d7bae8adad36e843588c0e0486c75af04
Lloyd <lloydbrownjnr@gmail.com>
2025-11-06 11:12:01 +0300

    functionality added to handle user authentication by name greetings.

--- parameters.py
+++ parameters.py
@@ -1,3 +1,12 @@
 # learning functions parameters in python
 # a parameter is a variable used to define a particular characteristic of a function or model.
 
+
+def greet_user(username):
+    """Display a simple greeting."""
+
+    print(f"Hello, {username}!")
+
+    greet_user("Allan")
+    greet_user("Maria")
+    greet_user("John")
