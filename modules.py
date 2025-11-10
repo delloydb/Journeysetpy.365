d19b1419f81c1882d40c0194d09643e7c511537b
Lloyd <lloydbrownjnr@gmail.com>
2025-11-10 12:24:23 +0300

    modules.py

---
+++ modules.py
@@ -0,0 +1,7 @@
+# learnig modules in python programming
+# A module is a file containing Python definitions and statements. The file name is the module name with the suffix .py added.
+# Modules are used to break down large programs into small manageable and
+# organized files. They also provide reusability of code.
+# We can define our most used functions in a module and import it, instead of copying their definitions into different programs.
+# Example of a module in Python
+
